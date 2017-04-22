ad9cf69cea5e3a0dc1714fbf89e51d3b89bf5cbc
Vaibhav Rai <iamvaibhavrai@gmail.com>
2017-04-22 11:24:28 +0530

    Kadane Algorithm

---
+++ Kadane's Algorithm/largest_contiguous_subarray.py
@@ -0,0 +1,18 @@
+def findLargestSubarray(a):
+    max_so_far = 0
+    max_ending_here = 0
+    for i in a:
+        max_ending_here += i
+        if max_ending_here < 0:
+            max_ending_here = 0
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+    return max_so_far
+
+def main():
+    a = [1,-2,3,-2,5]
+    result = findLargestSubarray(a)
+    print(result)
+
+if __name__ == '__main__':
+    main()
